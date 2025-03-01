927390e25c16bfef919aac149f6a0e31b74668a5
Thalison Bauer <thalisonbauer@hotmail.com>
2025-03-01 20:13:23 -0300

    index

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
-// Importa o Express
 const express = require('express');
 const path = require('path');
 
-// Cria a instância do aplicativo Express
 const app = express();
-
-// Define a porta em que o servidor vai rodar
 const port = 3000;
 
-// Configuração do EJS como motor de template
-app.set('view engine', 'ejs');
-
-// Define o diretório onde os arquivos EJS estão localizados
-app.set('views', path.join(__dirname, 'views'));
-
-// Serve os arquivos estáticos (CSS, JS, Imagens) da pasta 'public'
+// Serve arquivos estáticos como CSS, JS, Imagens da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota para a página principal (index.ejs)
+// Rota para servir o arquivo 'index.html'
 app.get('/', (req, res) => {
-  res.render('index');  // Renderiza o arquivo index.ejs da pasta views
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
-
-// Inicia o servidor e escuta na porta especificada
+// Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
